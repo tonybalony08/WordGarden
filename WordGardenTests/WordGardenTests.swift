546967de4bc78da111bc80de9c14fc675b7044c1
tonybalony08 <anthonycalixto08@gmail.com>
2026-01-12 11:24:52 -0800

    Initial Commit

---
+++ WordGardenTests/WordGardenTests.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenTests.swift
+//  WordGardenTests
+//
+//  Created by CALIXTO, ANTHONY on 1/12/26.
+//
+
+import Testing
+@testable import WordGarden
+
+struct WordGardenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
